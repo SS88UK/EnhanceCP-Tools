da756ca7cb624ed94b63365c9ed0da983b8aa43e
Steven Sullivan <17299026+SS88UK@users.noreply.github.com>
2023-03-16 13:46:19 -0400

    phpVersion version fix
    After updating to 9.1.0 Core

--- load.js
+++ load.js
@@ -169,7 +169,7 @@ function setupToolsPage() {
                 <td>${ W.appServerName } </td>
                 <td data-sortvalue="${ W.size }">${ formatBytes(W.size) }</td>
                 <td>${ D!==undefined ? (D.localRemote=='remote' ? 'Remote' : 'Local') : '' }</td>
-                <td>${ W.phpVersion!==null ? W.phpVersion.replace('php', '').replace(/(.{1})/,"$1.") : 'None' }</td>
+                <td>${ (W.phpVersion!==null && W.phpVersion!='' && W.phpVersion!==undefined) ? W.phpVersion.replace('php', '').replace(/(.{1})/,"$1.") : 'None' }</td>
                 <td>${ W.backupServerName!==undefined ? W.backupServerName : 'None' } ${ W.pendingBackup!==null ? '<abbr title="Pending Backup">(PB)</abbr>' : '' }</td>
                 <td>${ W.emailServerName!==undefined ? W.emailServerName : 'None' }</td>
             </tr>
